05fe6eb83429caf6abe05c51cbdf9b7932a2dbec
RubenGiC <rubengirela@gmail.com>
2021-05-21 19:45:52 +0200

    fixed some errors to normalizer and partition samples

--- P3/practica3.py
+++ P3/practica3.py
@@ -9,7 +9,7 @@ import numpy as np
 #para abrir el archivo csv
 import csv
 #para normalizar los datos
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
 
 # para visualizar los datos en una grafica necesitamos:
 import pandas as pd # crea el formato 2D
@@ -56,10 +56,6 @@ def readData(archivo, tipo):
                 datos.append(data)
             datos = np.array(datos, dtype='f4')
             
-            #normalizamos los datos
-            normalizer = Normalizer().fit(datos)
-            datos = normalizer.transform(datos)
-            
             #Y separamos los datos de las etiquetas
             x = datos
             y = np.array(y, dtype='f4') #datos[:, datos.shape[1]-1:datos.shape[1]]
@@ -81,10 +77,6 @@ def readData(archivo, tipo):
             #indicamos el formato de los datos de tipo float con 4 decimales
             datos = np.array(datos, dtype='f4')
             
-            #normalizamos los datos
-            normalizer = Normalizer().fit(datos)
-            datos = normalizer.transform(datos)
-            
             #Y separamos los datos de las etiquetas
             x = datos[:, :-1]
             y = datos[:, datos.shape[1]-1:datos.shape[1]]
@@ -93,63 +85,6 @@ def readData(archivo, tipo):
 
 x_class,y_class = readData('datos/clasificacion/Sensorless_drive_diagnosis.txt','txt')
 x_reg,y_reg = readData('datos/regresion/train.csv','csv')
-
-
-# #MOSTAR GRAFICA 2D de los datos de la clasificación
-# #transformamos los datos de kd a 2d
-# pca = PCA(n_components=2)#indicamos la dimensionalidad
-# #transformamos los datos
-# principalComponents = pca.fit_transform(x_class)
-# #creamos la estructura para una visualización correcta de los datos en 2D
-# principalXlabel = pd.DataFrame(data = principalComponents, columns = ['componente 1', 'componente 2'])
-# #esto es para la etiqueta
-# pca = PCA(n_components=1)#indicamos la dimensionalidad
-# #y creamos la estructura con el mismo formato
-# principalYlabel = pd.DataFrame(data = y_class, columns = ['target'])
-
-# #concatenamos los datos y las etiquetas
-# finalDf = pd.concat([principalXlabel, principalYlabel], axis=1)
-
-# print('DATOS PARA LA CLASIFICACIÓN')
-# print(finalDf)
-
-# #Visualización grafica,
-# fig = plt.figure(figsize=(8,8))
-# ax = fig.add_subplot(1,1,1)
-# ax.set_xlabel('componente 1')
-# ax.set_ylabel('componente 2')
-# ax.set_title('Datos para la Clasificación')
-# ax.scatter(finalDf['componente 1'],finalDf['componente 2'],c=finalDf['target'])
-# fig.show()
-
-
-# #MOSTAR GRAFICA 2D de los datos para la regresión
-# #transformamos los datos de kd a 2d
-# pca = PCA(n_components=2)#indicamos la dimensionalidad
-# #transformamos los datos
-# principalComponents = pca.fit_transform(x_reg)
-# #creamos la estructura para una visualización correcta de los datos en 2D
-# principalXlabel = pd.DataFrame(data = principalComponents, columns = ['componente 1', 'componente 2'])
-# #esto es para la etiqueta
-# pca = PCA(n_components=1)#indicamos la dimensionalidad
-# #y creamos la estructura con el mismo formato
-# principalYlabel = pd.DataFrame(data = y_reg, columns = ['target'])
-
-# #concatenamos los datos y las etiquetas
-# finalDf = pd.concat([principalXlabel, principalYlabel], axis=1)
-# print('DATOS PARA LA REGRESIÓN')
-# print(finalDf)
-
-# #Visualización grafica,
-# fig = plt.figure(figsize=(8,8))
-# ax = fig.add_subplot(1,1,1)
-# ax.set_xlabel('componente 1')
-# ax.set_ylabel('componente 2')
-# ax.set_title('Datos para la Regresión')
-# ax.scatter(finalDf['componente 1'],finalDf['componente 2'],c=finalDf['target'])
-# fig.show()
-
-# input("\n--- Pulsar tecla para continuar ---\n")
 
 #divido la muestra total en 2 muestras una para el training y otra para el test
 #Para ello crearemos 2 listas de indices que se barajaran y en principio 
@@ -203,28 +138,103 @@ y_training_reg = np.array(y_training_reg)
 x_test_reg = np.array(x_test_reg)
 y_test_reg = np.array(y_test_reg)
 
+#normalizamos los datos
+
+scaler = StandardScaler()
+
+x_training_class = scaler.fit_transform(x_training_class)
+x_test_class = scaler.fit_transform(x_test_class)
+
+x_training_reg = scaler.fit_transform(x_training_reg)
+x_test_reg = scaler.fit_transform(x_test_reg)
+
 
 print("Numero de datos para el training (clasificacion): ",y_training_class.size)
 print("Numero de datos para el test (clasificacion): ",y_test_class.size)
 print("Numero de datos para el training (regresión): ",y_training_reg.size)
 print("Numero de datos para el test (regresion): ",y_test_reg.size)
 
-# #RidgeClassifier
-# #creamos nuestro modelo
-# logistic = RidgeClassifier().fit(x_training_class, y_training_class)
-# #lo probamos con el conjunto de test
-# predicted = logistic.predict(x_test_class)
-# #y comprobamos como de buenos son los resultados
-# result = classification_report(y_test_class,predicted)
 
-# #Mostrarmos los resultados
-# print("RidgeClassifier:")
-# print("\tEin: ",accuracy_score(y_training_class, logistic.predict(x_training_class)))
-# print("\tEtest: ",accuracy_score(y_test_class, logistic.predict(x_test_class)))
-# print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
-# print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
-# print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
-# print("\t Tabla resultados:\n",result)
+
+
+#MOSTAR GRAFICA 2D de los datos de la clasificación
+#transformamos los datos de kd a 2d
+pca = PCA(n_components=2)#indicamos la dimensionalidad
+#transformamos los datos
+principalComponents = pca.fit_transform(x_training_class)
+#creamos la estructura para una visualización correcta de los datos en 2D
+principalXlabel = pd.DataFrame(data = principalComponents, columns = ['componente 1', 'componente 2'])
+#esto es para la etiqueta
+pca = PCA(n_components=1)#indicamos la dimensionalidad
+#y creamos la estructura con el mismo formato
+principalYlabel = pd.DataFrame(data = y_training_class, columns = ['target'])
+
+#concatenamos los datos y las etiquetas
+finalDf = pd.concat([principalXlabel, principalYlabel], axis=1)
+
+print('DATOS PARA LA CLASIFICACIÓN')
+#print(finalDf)
+
+for i in finalDf['componente 1']:
+    if(i>20):
+        print(i)
+
+#Visualización grafica,
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('componente 1')
+ax.set_ylabel('componente 2')
+ax.set_title('Datos Training para la Clasificación')
+ax.scatter(finalDf['componente 1'],finalDf['componente 2'],c=finalDf['target'])
+fig.show()
+
+
+# #MOSTAR GRAFICA 2D de los datos para la regresión
+# #transformamos los datos de kd a 2d
+# pca = PCA(n_components=2)#indicamos la dimensionalidad
+# #transformamos los datos
+# principalComponents = pca.fit_transform(x_training_reg)
+# #creamos la estructura para una visualización correcta de los datos en 2D
+# principalXlabel = pd.DataFrame(data = principalComponents, columns = ['componente 1', 'componente 2'])
+# #esto es para la etiqueta
+# pca = PCA(n_components=1)#indicamos la dimensionalidad
+# #y creamos la estructura con el mismo formato
+# principalYlabel = pd.DataFrame(data = y_training_reg, columns = ['target'])
+
+# #concatenamos los datos y las etiquetas
+# finalDf = pd.concat([principalXlabel, principalYlabel], axis=1)
+# print('DATOS PARA LA REGRESIÓN')
+# print(finalDf)
+
+# #Visualización grafica,
+# fig = plt.figure(figsize=(8,8))
+# ax = fig.add_subplot(1,1,1)
+# ax.set_xlabel('componente 1')
+# ax.set_ylabel('componente 2')
+# ax.set_title('Datos Training para la Regresión')
+# ax.scatter(finalDf['componente 1'],finalDf['componente 2'],c=finalDf['target'])
+# fig.show()
+
+# input("\n--- Pulsar tecla para continuar ---\n")
+
+
+
+#RidgeClassifier
+#creamos nuestro modelo
+logistic = RidgeClassifier().fit(x_training_class, y_training_class)
+#lo probamos con el conjunto de test
+predicted = logistic.predict(x_test_class)
+#y comprobamos como de buenos son los resultados
+result = classification_report(y_test_class,predicted)
+
+#Mostrarmos los resultados
+print("RidgeClassifier:")
+print("\tEin: ",accuracy_score(y_training_class, logistic.predict(x_training_class)))
+print("\tEtest: ",accuracy_score(y_test_class, logistic.predict(x_test_class)))
+print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
+print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
+print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
+print("\tTabla resultados:\n",result)
 
 # input("\n--- Pulsar tecla para continuar ---\n")
 
@@ -244,29 +254,47 @@ print("Numero de datos para el test (regresion): ",y_test_reg.size)
 # print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
 # print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
 # print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
-# print("\t Tabla resultados:\n",result)
+# print("\tTabla resultados:\n",result)
 
 # input("\n--- Pulsar tecla para continuar ---\n")
 
-#Perceptron
-#creamos nuestro modelo
-logistic = Perceptron().fit(x_training_class, y_training_class.ravel())
-#clasificamos el conjunto test con nuestro modelo
-predicted = logistic.predict(x_test_class)
-#y comprobamos como de buenos son los resultados
-result = classification_report(y_test_class,predicted)
+# #Perceptron
+# #creamos nuestro modelo
+# logistic = Perceptron().fit(x_training_class, y_training_class.ravel())
+# #clasificamos el conjunto test con nuestro modelo
+# predicted = logistic.predict(x_test_class)
+# #y comprobamos como de buenos son los resultados
+# result = classification_report(y_test_class,predicted)
 
-#Mostrarmos los resultados
-print("Perceptron:")
-print("\tEin: ",accuracy_score(y_training_class, logistic.predict(x_training_class)))
-print("\tEtest: ",accuracy_score(y_test_class, logistic.predict(x_test_class)))
-print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
-print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
-print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
-print("\t Tabla resultados:\n",result)
+# #Mostrarmos los resultados
+# print("Perceptron:")
+# print("\tEin: ",accuracy_score(y_training_class, logistic.predict(x_training_class)))
+# print("\tEtest: ",accuracy_score(y_test_class, logistic.predict(x_test_class)))
+# print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
+# print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
+# print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
+# print("\tTabla resultados:\n",result)
 
+# input("\n--- Pulsar tecla para continuar ---\n")
 
+# #SGD (STOCHASTIC GRADIENT DESCENT)
+# #creamos nuestro modelo
+# logistic = SGDClassifier().fit(x_training_class, y_training_class.ravel())
+# #clasificamos el conjunto test con nuestro modelo
+# predicted = logistic.predict(x_test_class)
+# #y comprobamos como de buenos son los resultados
+# result = classification_report(y_test_class,predicted)
 
+# #Mostrarmos los resultados
+# print("SGD (Gradiente Descendiente Estocástico):")
+# print("\tEin: ",accuracy_score(y_training_class, logistic.predict(x_training_class)))
+# print("\tEtest: ",accuracy_score(y_test_class, logistic.predict(x_test_class)))
+# print("\tEout: ",accuracy_score(y_class, logistic.predict(x_class)))
+# print("\tPrecisión media training: ",logistic.score(x_training_class,y_training_class))
+# print("\tPrecisión media test: ",logistic.score(x_test_class,y_test_class))
+# print("\tTabla resultados:\n",result)
+
+# input("\n--- Pulsar tecla para continuar ---\n")
 
 
 
